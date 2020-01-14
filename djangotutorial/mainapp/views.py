4f30964b267f0b4decbdaf109febf068c758bc50
saruman <saruman@localhost.localdomain>
2020-01-13 22:39:16 -0600

    a

---
+++ djangotutorial/mainapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+# Create your views here.
+
+class Home(View):
+    def get(self, request):
+        # <view logic>
+        return HttpResponse('result')
